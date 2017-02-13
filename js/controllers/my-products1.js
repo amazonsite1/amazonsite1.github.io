92b984bbacd183894c6af6b470fb7fe3ce18f22d
amazonsite1 <idoesamazon@gmail.com>
2017-02-13 18:19:16 -0500

    format?

--- js/controllers/my-products1.js
+++ js/controllers/my-products1.js
@@ -1,5 +1,6 @@
 app.controller('Products1Controller', ['$scope', '$http', function($scope, $http){
 	$http.get('js/products1.json').then(function(products){
 	$scope.myProducts = products.data;
+	console.log($scope.myProducts);
 	});
 }]);
