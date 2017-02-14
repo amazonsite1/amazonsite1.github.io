5b755b66fd0619f064428b872502331b2c1292f6
amazonsite1 <idoesamazon@gmail.com>
2017-02-14 17:30:04 -0500

    top 4

---
+++ js/components/products-four-component.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp');
+
+app.component('productsOne',{
+	templateUrl: '../views/products4.html',
+	controller: "Products4Controller"
+});
---
+++ js/components/products-three-component.js
@@ -0,0 +1,6 @@
+var app = angular.module('myApp');
+
+app.component('productsOne',{
+	templateUrl: '../views/products3.html',
+	controller: "Products3Controller"
+});
