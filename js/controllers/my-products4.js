37c7110e756955d450bda8324950f9c6e07ceb62
amazonsite1 <idoesamazon@gmail.com>
2017-02-13 21:06:56 -0500

    comp

--- js/controllers/my-products4.js
+++ js/controllers/my-products4.js
@@ -1,6 +1,5 @@
 app.controller('Products4Controller', ['$scope', '$http', function($scope, $http){
 $http.get('js/products4.json').then(function(products){
 	$scope.myProducts = products.data;
-	console.log($scope.myProducts);
 	});
 }]);
