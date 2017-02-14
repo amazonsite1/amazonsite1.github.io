633cd2c9048d063cd7e252fde990d711f8f079e5
amazonsite1 <idoesamazon@gmail.com>
2017-02-14 17:46:07 -0500

    components

--- js/app.js
+++ js/app.js
@@ -5,20 +5,7 @@ app.config(function($routeProvider){
 		templateUrl : "views/landing.html"
 	})
 	.when("/products", {
-		templateUrl : "views/products.html",
-		controller: "Products1Controller"
-	})
-	.when("/products2", {
-		templateUrl : "views/products2.html",
-		controller: "Products2Controller"
-	})
-	.when("/products3", {
-		templateUrl : "views/products3.html",
-		controller: "Products3Controller"
-	})
-	.when("/products4", {
-		templateUrl : "views/products4.html",
-		controller: "Products4Controller"
+		templateUrl : "views/products-home.html"
 	})
 	.when("/about-us", {
 		templateUrl : "views/about-us.html"
