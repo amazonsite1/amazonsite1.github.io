16a75dfa626bc80453d9fb8cfd6b7eeb14f8b25f
amazonsite1 <idoesamazon@gmail.com>
2017-02-13 23:01:52 -0500

    I DID IT?!

--- js/controllers/my-products1.js
+++ js/controllers/my-products1.js
@@ -2,5 +2,4 @@ app.controller('Products1Controller', ['$scope', '$http', function($scope, $http
 	$http.get('js/products1.json').then(function(products){
 	$scope.myProducts = products.data;
 	});
-	$scope.prodLoad = true;
 }]);
