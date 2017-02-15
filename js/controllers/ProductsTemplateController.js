c43115b46ebfc6e12fcd68aff28c864c294a183e
amazonsite1 <idoesamazon@gmail.com>
2017-02-14 21:44:24 -0500

    titles test run

--- js/controllers/ProductsTemplateController.js
+++ js/controllers/ProductsTemplateController.js
@@ -1,6 +1,8 @@
 app.controller('ProductsTemplateController', ['$scope', '$http', function($scope, $http){
 	$scope.view = "one";
-
+	$http.get('js/product-titles.json').then(function(titles){
+	$scope.productTitles = titles.data;
+	});
 	$scope.prodView = function(view){
 		$scope.view = view;
 	};
